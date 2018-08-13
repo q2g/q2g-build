819760f84852da6a7338321913cafe62481a700d
Konrad Mattheis <konrad.mattheis@akquinet.de>
2018-08-13 15:31:50 +0200

    remove bin

--- bin/api/builder.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Builders;
-(function (Builders) {
-    Builders["TYPESCRIPT"] = "tsc";
-    Builders["EXTENSION"] = "extension";
-    Builders["WEBPACK"] = "webpack";
-})(Builders = exports.Builders || (exports.Builders = {}));
--- bin/builder/qext-file-builder/data/exception/qext-config.exception.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class QextConfigurationException extends Error {
-    constructor(msg = "") {
-        super(msg);
-        Object.setPrototypeOf(this, QextConfigurationException.prototype);
-    }
-}
-exports.QextConfigurationException = QextConfigurationException;
